0214590f1c14e36f75d9ae1542d080a9c9973fe0
frost (frostg-012) <optimus7907@gmail.com>
2021-12-12 07:50:13 +0700

    juice: overlay-nad: Set Nusantara maintainer name
    Co-authored-by: I-n-o-k <inok.dr189@gmail.com>
    Change-Id: I8e6030acd86760bf0c22a28364b3af45cb9c8cb7

--- device.mk
+++ device.mk
@@ -90,7 +90,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-nad
 
 # Power
 PRODUCT_PACKAGES += \
