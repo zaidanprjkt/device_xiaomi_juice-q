534c087d4927d013222f590f0118dd80f87a0a83
Peter Cai <peter@typeblog.net>
2022-02-01 13:02:54 +0700

    juice: Panic to recovery not bootloader
    Change-Id: Iaf737890e3c860df504629d35a4a68c494f39910

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,6 +89,7 @@ BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1 lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048 loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
