2c0341ee1fb011685134ea378b632a96b5eca5ca
Lokesh Gidra <lokeshgidra@google.com>
2022-02-05 10:27:11 +0700

    juice: Use 6GB dalvik-heap config
    Change-Id: I9f1a147bf62f4da45aecc6b28aa29794275ae0e9

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService
