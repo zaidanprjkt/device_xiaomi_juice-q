Night-Raids-Reborn/diemen

juice: Initial juice device tree
Change-Id: I31c09c7541c3fae7997d7c999a4119e5fd07c1f0

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2021-2022 frstprjkt
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_juice.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_juice-eng \
+    aosp_juice-userdebug \
+    aosp_juice-user

zaidanprjkt/device_xiaomi_juice-q

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2021-2022 frstprjkt
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/nad_juice.mk
+
+COMMON_LUNCH_CHOICES := \
+    nad_juice-eng \
+    nad_juice-userdebug \
+    nad_juice-user