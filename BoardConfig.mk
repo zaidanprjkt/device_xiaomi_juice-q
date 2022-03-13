26f08b1b04bfbdf470ab4be3516d451af287471d
frost (frostg-012) <optimus7907@gmail.com>
2022-03-13 21:18:45 +0700

    juice: Switch to Enforcing build
    Change-Id: I47d375053c269b69296c9d1d9e7352e50646665b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,7 +88,6 @@ BOARD_KERNEL_CMDLINE += androidboot.memcg=1 lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048 loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SECOND_OFFSET := 0xf00000
