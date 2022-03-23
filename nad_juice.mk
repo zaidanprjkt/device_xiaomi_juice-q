cee733686593cfde5ae132c3528506cb2bf0249e
frost (frostg-012) <optimus7907@gmail.com>
2022-03-23 09:26:37 +0700

    nad: Use pixel charging
    Change-Id: Ibd26ca328ae280a86d599da1e4afb1a2690ad681

--- nad_juice.mk
+++ nad_juice.mk
@@ -11,6 +11,7 @@ $(call inherit-product, device/xiaomi/juice/device.mk)
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 NAD_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
+USE_PIXEL_CHARGING := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := nad_juice
